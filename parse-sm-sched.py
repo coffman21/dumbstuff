6fb37eb82b2c12cb9b3bc8bae9b49be07705e448
coffman21 <fire2low@rambler.ru>
2018-06-05 02:44:45 +0300

    some more labunetz goes on

--- parse-sm-sched.py
+++ parse-sm-sched.py
@@ -1,14 +1,15 @@
 # coding=utf-8
-from bs4 import BeautifulSoup
-import requests
-from lxml import etree
-import re
 import ast
+import re
+import requests
+from time import sleep
 
 URL = "http://www.bmstu.ru/mstu/undergraduate/schedule/table-view/"
-for i in range(337,572):
-    print("parsed {} page:".format(i))
 
+
+def parse(i):
+    print("parsed {} page:".format(i))
+    sleep(1)
     init_r = r = requests.get(URL)
     init_r.encoding = 'utf-8'
     r = requests.post(URL, {"id": i, "action": "view"})
@@ -17,10 +18,16 @@ for i in range(337,572):
     bmstu_rasp = ''.join(r.text.splitlines())
 
     regexp = '(\$container\.handsontable\(\{\s*data:\s*)(.*)(\,\s*colWidths: )'
+    print(bmstu_rasp)
     splitted = re.search(regexp, bmstu_rasp).group(2)
-    table_list = ast.literal_eval(splitted)
-    for tr in table_list:
-        for td in tr:
-            if "Лабунец" in td:
-                print(td)
+    if "Лабунец" in splitted:
+        table_list = ast.literal_eval(splitted)
+        for tr in table_list:
+            for td in tr:
+                if "Лабунец" in td:
+                    print(td)
+
+
+for i in range(337,572):
+    parse(i)
 
