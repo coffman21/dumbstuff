8f1738e9b750e888255b46c8f87184e4fdc532a6
coffman21 <fire2low@rambler.ru>
2018-06-05 02:48:41 +0300

    more labunetz

--- parse-sm-sched.py
+++ parse-sm-sched.py
@@ -9,16 +9,14 @@ URL = "http://www.bmstu.ru/mstu/undergraduate/schedule/table-view/"
 
 def parse(i):
     print("parsed {} page:".format(i))
-    sleep(1)
-    init_r = r = requests.get(URL)
-    init_r.encoding = 'utf-8'
+
     r = requests.post(URL, {"id": i, "action": "view"})
     r.encoding = 'utf-8'
 
     bmstu_rasp = ''.join(r.text.splitlines())
 
     regexp = '(\$container\.handsontable\(\{\s*data:\s*)(.*)(\,\s*colWidths: )'
-    print(bmstu_rasp)
+
     splitted = re.search(regexp, bmstu_rasp).group(2)
     if "Лабунец" in splitted:
         table_list = ast.literal_eval(splitted)
