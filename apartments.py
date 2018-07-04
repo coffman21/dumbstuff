f56b0b7d90935467de258568b72d242842782807
coffman21 <fire2low@rambler.ru>
2018-07-04 15:39:16 +0300

    apartments: welcome route cost

--- apartments.py
+++ apartments.py
@@ -2,12 +2,12 @@
 import ast
 import re
 import requests
-from transliterate import translit, get_available_language_codes
 from pprint import pprint
 from time import sleep
 
 SITE = "https://www.domofond.ru"
 URL = "https://www.domofond.ru/arenda-kvartiry-moskva-c3584"
+
 PARAMS = {
     "PriceTo": "40000",
     "RentalRate": "Month",
@@ -29,14 +29,32 @@ REGS = {
     "distance": '(<\/span> )([^<]+)(<\/strong>\s*<\/div>\s*<div class="op-tile-number">)',
 }
 
+# param: term: Комсомольская
+# reply format: [{'label': 'komsomolskaya_sokolnicheskaya', ...}]
+GET_NAME_QUERY = 'http://metro.mwmoskva.ru/moscow-map/stList.v2.php'
+
+# params:
+# start: fonvizinskaya_ljublinsko-dmitrovskaja
+# end: cherkizovskaya_sokolnicheskaya
+# reply format: {'time_count': 33, ...}
+GET_DISTANCE_QUERY = "http://metro.mwmoskva.ru/moscow-map/findWay.v2.php"
+
+# feel free to edit
+FAV_STATIONS = {
+    'Комсомольская': 'komsomolskaya_sokolnicheskaya',
+    'Университет': 'universitet_sokolnicheskaya',
+    'Павелецкая': 'paveletskaya_kolcevaya',
+    'Курская': 'kurskaya_arbatsko-pokrovskaya',
+    'Пролетарская': 'proletarskaya_tagansko-krasnopresnenskaya',
+}
+
 PARSED = {}
 
 def parsePagination(i):
     print("parsed {} page:".format(i))
     new_par = PARAMS
     new_par["Page"] = i
-    r = requests.get(URL, new_par)
-    r.encoding = 'utf-8'
+    r = send_request(URL, new_par)
 
     main_page = ''.join(r.text.splitlines())
 
@@ -58,20 +76,48 @@ def parsePagination(i):
                 PARSED[cur_len+idx][key] = prop[1].lstrip('Комиссия ').rstrip('%')
             else:
                 PARSED[cur_len+idx][key] = prop[1]
-    print(PARSED)
+
+    for apt_num in range(l):
+        metro = PARSED[apt_num+cur_len]['station']
+        dir_costs = get_metro_stations_cost(metro)
+        for k, v in dir_costs.items():
+            PARSED[apt_num+cur_len][k] = v
+        print(dir_costs.values())
+        PARSED[apt_num+cur_len]['sum_time'] = sum((int(_) if type(_) == str else 0 for _ in dir_costs.values()))
+
 
 def parse_all():
     for i in range(1,9):
         parsePagination(i)
         sleep(1)
 
-METRO_PARAMS = {}
-
-GET_NAME_QUERY = 'http://metro.mwmoskva.ru/moscow-map/stList.v2.php?term='
-"http://metro.mwmoskva.ru/moscow-map/findWay.v2.php?start=fonvizinskaya_ljublinsko-dmitrovskaja&end=cherkizovskaya_sokolnicheskaya"
 
 def get_metro_stations_cost(name):
-    pass
+    r = send_request(GET_NAME_QUERY, {
+        'term': name
+    })
+    trans_name = r.json()[0]['label']
+    etas = {}
+    print(name)
+    for k, v in FAV_STATIONS.items():
+        r = send_request(GET_DISTANCE_QUERY, {
+            'start': trans_name,
+            'end': v
+        })
+        eta = r.json()['time_count']
+        etas[k] = eta
+        sleep(0.1)
+    return etas
+
+def send_request(url, params):
+    r = requests.get(url, params)
+    if r.status_code != 200:
+        sleep(1)
+        return send_request(url, params)
+
+    else:
+        r.encoding = 'utf-8'
+        return r
 
 
 parse_all()
